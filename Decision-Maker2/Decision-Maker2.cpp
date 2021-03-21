d2727a7ced0e6eb6a00c8254b0cb41219a69cd9d
Vovasch <zhyvotivskyi.vova@gmail.com>
2021-03-21 22:51:51 +0200

    First commit

---
+++ Decision-Maker2/Decision-Maker2.cpp
@@ -0,0 +1,20 @@
+﻿//#pragma comment(lib, "mlpack.lib")
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <stdio.h>
+//#include <mlpack/prereqs.hpp>
+//#include "mlpack_main.hpp" // Щоб його підключити потрібно в ньому закоментувани дві строки, які викликають помилки
+#include <mlpack/core/util/io.hpp>
+#include <mlpack/methods/gmm/gmm.hpp>
+#include <utility>
+#include <vector>
+
+int main()
+{
+
+
+
+	system("Pause");
+	return 0;
+}
